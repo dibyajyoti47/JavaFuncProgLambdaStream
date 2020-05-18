ebfeac2d00ded5dc41a3135b7caa47f0f8ade270
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2020-05-18 16:05:46 +0530

    initiaal 2

--- src/programming/FPO1Structured.java
+++ src/programming/FPO1Structured.java
@@ -1,10 +1,28 @@
 package programming;
 
+import java.util.List;
+
 public class FPO1Structured {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//	printAllNumbersInListStructured(List.of(1, 2, 3, 4, 21, 32, 23, 44, 51, 87, 34, 43));
+		printAllNumbersInFunctionalApproach(List.of(1, 2, 3, 4, 21, 32, 23, 44, 51, 87, 34, 54));
+	}
+	
+	private static void printAllNumbersInListStructured(List<Integer> numbers) {
 
+		for (int number : numbers) { //traditional approach
+			System.out.println(number);
+		}
+	}
+	
+	private static void print (int number) {
+		System.out.println(number);
+	}
+	
+	private static void printAllNumbersInFunctionalApproach(List<Integer> numbers) {
+		numbers.stream()
+			.forEach(System.out :: println);
 	}
 
 }
